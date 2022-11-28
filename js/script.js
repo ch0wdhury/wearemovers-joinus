d9d76ac3f5579f040fd1c9b65d6abd4206f2f1e3
Ashiq Ali Chowdhury <43188066+ch0wdhury@users.noreply.github.com>
2022-11-29 04:15:32 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(e){
+    		
+			$('.img-check').click(function(e) {
+        $('.img-check').not(this).removeClass('check')
+    		.siblings('input').prop('checked',false);
+    	$(this).addClass('check')
+            .siblings('input').prop('checked',true);
+    });
+			
+	});
+
+$(document).ready(function() {
+     $('#msg-wrap').hide();//on cache par defaut les messages
+    $('.conversation').click(function() {
+        var id = this.id.substring(5);//ici on recupere les 5 premiers caracteres de l'id qui sont menu_ et numero de l'id genre menu_1
+        $(this).siblings('.conversation').removeClass('active');//on cache le active des autres cases quand on clique sur un des messages de gauche 
+        $(this).addClass('active');// on place la couleur bleu active sur la case cliquée
+        // on cache les messages dont l'id est différente de celle courante et on montre celle que l'on a recup
+        $('[id^=message_]').hide();
+        $('#message_'+id).show();
+        $('#msg-wrap').show();
+    });  
+});
